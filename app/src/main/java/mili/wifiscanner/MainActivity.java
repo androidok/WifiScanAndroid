e664e30199518e440cbb74774a62572694b01ca0
Ming <mili@csm-wl-dhcp-196-119.mines.edu>
2020-11-22 03:11:04 -0700

    Finish the settings on intervals and roomNum

--- app/src/main/java/mili/wifiscanner/MainActivity.java
+++ app/src/main/java/mili/wifiscanner/MainActivity.java
@@ -10,6 +10,7 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
@@ -17,9 +18,13 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SoundEffectConstants;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButtonToggleGroup;
@@ -41,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     private static CharSequence mDataType = "unknown";
     private static int mRoomNum = 4;
     private static CharSequence mRoomID = "unknown";
+    private static int mUserInput = 0;
+    private static int mSettingID = -1;
 
     private WifiManager mWifiManager;
     private WifiScanReceiver mWifiScanReceiver;
@@ -48,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Handler mHandler;
     private Runnable mRunnable;
-    private static int mDelay = 30000; // 1000 milliseconds == 1 second
+    private static int mInterval = 30000; // 1000 milliseconds == 1 second
     private static boolean mScanStarted = false;
 
     private DataWriter mDataWriter;
@@ -129,39 +136,27 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG, "Scan once...");
                     logToUi(getString(R.string.retrieving_access_points));
                     mWifiManager.startScan();
-                    mHandler.postDelayed(this, mDelay);
+                    mHandler.postDelayed(this, mInterval);
                 } else {
                     stopWifiScanner();
                 }
             }
         };
-
-
     }
 
     private void startWifiScanner() {
         if (mRoomToggle.getCheckedChipId() == ChipGroup.NO_ID) {
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setMessage(R.string.start_warning)
-                            .setPositiveButton("OK", null)
-                            .show();
-                }
-            });
+            new AlertDialog.Builder(MainActivity.this)
+                    .setMessage(R.string.start_warning)
+                    .setPositiveButton(R.string.dialog_positive, null)
+                    .show();
             mScanTextView.callOnClick();
         } else {
             mDataWriter = new DataWriter(mDataType, getString(R.string.app_name));
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setMessage(mDataWriter.info())
-                            .setPositiveButton("OK", null)
-                            .show();
-                }
-            });
+            new AlertDialog.Builder(MainActivity.this)
+                    .setMessage(mDataWriter.info())
+                    .setPositiveButton(R.string.dialog_positive, null)
+                    .show();
             mHandler.post(mRunnable);
         }
     }
@@ -194,6 +189,42 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(mWifiScanReceiver);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.pick_setting_item)
+                    .setItems(R.array.items_array, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            mSettingID = which;
+                            changeSettingItem();
+                        }
+                    }).show();
+        } else if (id == R.id.action_help) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage(R.string.help_info)
+                            .setPositiveButton(R.string.dialog_positive, null)
+                            .show();
+                }
+            });
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
     private class WifiScanReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -201,10 +232,10 @@ public class MainActivity extends AppCompatActivity {
                 mAccessPoints = mWifiManager.getScanResults();
                 mAdapter.swapData(mAccessPoints);
                 mAdapter.notifyDataSetChanged();
-                logToUi( getString(R.string.stop_scan_info)
+                logToUi(getString(R.string.stop_scan_info)
                         + "\n" + mAccessPoints.size()
                         + " APs discovered.\nCurrent scan delay: "
-                        + (mDelay/1000) + "s"
+                        + (mInterval/1000.0) + "s"
                 );
                 mDataWriter.writeToFiles(mRoomID, mAccessPoints);
             }
@@ -219,6 +250,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createRoomChips() {
+        int childCount = mRoomToggle.getChildCount();
+        Log.d(TAG, childCount + " children");
+        for(int i=childCount-1; i>0; i--) {
+            View currentChild = mRoomToggle.getChildAt(i);
+            // Change ImageView with your desired type view
+            if (currentChild instanceof Chip) {
+                mRoomToggle.removeView(currentChild);
+            }
+        }
         for (int i = 0; i<mRoomNum; i++) {
             Chip chip = new Chip(mRoomToggle.getContext());
             chip.setText(String.valueOf(i+1));
@@ -227,4 +267,64 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void changeSettingItem() {
+        String[] items = getResources().getStringArray(R.array.items_array);
+        String settingItem = items[mSettingID];
+        Log.d(TAG,  settingItem + " is chosen");
+        // get dialog_setting layout view
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        View dialogView = inflater.inflate(R.layout.dialog_setting, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                MainActivity.this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(dialogView);
+
+        final TextView textView = dialogView
+                .findViewById(R.id.setting_item_text_view);
+        textView.setText(settingItem);
+
+
+        final EditText userInput = (EditText) dialogView
+                .findViewById(R.id.setting_item_text_edit);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton(R.string.dialog_positive,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                String input = userInput.getText().toString();
+                                if (input.isEmpty()) {
+                                    dialog.cancel();
+                                } else {
+                                    mUserInput = Integer.parseInt(input);
+                                    Log.d(TAG, "User Input Value: " + mUserInput);
+                                    if (mSettingID == 0) {
+                                        mRoomNum = mUserInput;
+                                        createRoomChips();
+                                    } else if (mSettingID == 1) {
+                                        mInterval = mUserInput * 1000;
+                                        mHandler.removeCallbacks(null);
+                                        Log.d(TAG, "User Input Value: " + mInterval);
+                                    }
+                                }
+                            }
+                        })
+                .setNegativeButton(R.string.dialog_negative,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
+
+    }
 }
